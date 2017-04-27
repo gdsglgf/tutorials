1c178d8354d3d0e8ec862c6752b6bba2d843d270
gdsglgf <wzguofulgz@126.com>
2017-04-27 20:14:35 +0800

    add spider demo

--- python/util/urllib_util.py
+++ python/util/urllib_util.py
@@ -13,3 +13,40 @@ def get_content(url):
 
 	return content
 
+
+#! /usr/bin/env python2
+import urllib 
+import urllib2 
+url = 'http://www.pythontab.com' 
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' 
+  
+values = {'name' : 'Michael Foord', 
+          'location' : 'pythontab', 
+          'language' : 'Python' } 
+headers = { 'User-Agent' : user_agent } 
+data = urllib.urlencode(values) 
+req = urllib2.Request(url, data, headers) 
+response = urllib2.urlopen(req) 
+the_page = response.read()
+
+
+
+#! /usr/bin/env python3
+import urllib.parse
+import urllib.request
+
+url = 'http://localhost/login.php'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+values = {
+'act' : 'login',
+'login[id]' : '123456',
+'login[password]' : '123456'
+}
+headers = { 'User-Agent' : user_agent }
+
+data = urllib.parse.urlencode(values)
+req = urllib.request.Request(url, data, headers)
+response = urllib.request.urlopen(req)
+the_page = response.read()
+
+print(the_page.decode("utf8"))
